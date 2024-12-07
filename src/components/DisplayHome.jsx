bbc8ee6f55934bba062b8024cd6dffdc1a832459
Kush <reach2ayushgupta@gmail.com>
2024-12-08 02:13:08 +0530

    need to fix the alignment of list of music

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,9 +1,17 @@
 import React from "react"
 import Navbar from "./Navbar"
+import { albumsData } from "../assets/assets"
+import AlbumItem from "./Albumitem"
 const DisplayHome=()=>{
     return(
         <>
         <Navbar/>
+        <div className="mb-4">
+            <h1 className="my-5 font-bold text-2xl">Features Charts</h1>
+            <div className="flex overflow-auto">
+            {albumsData.map((item,index)=>(<AlbumItem key={index} name={item.name}desc={item.desc} id={item.id} image={item.image}/>))}
+        </div>
+        </div>
         </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,8 +11,16 @@ const Navbar=()=>{
             </div>
             <div className="flex items-center gap-4 absolute top-4 right-4">
                 <p className="bg-white text-black text-[13px] px-4 py-1 rounded-2*1 hidden md:block cursor-pointer text-black rounded-full mt-4">Explore Premium</p>
+                <p className="bg-black py-1 px-4 rounded-2*1 text-[13px] text-white cursor-pointer">Install App</p>
+                <p className="bg-blue-500 text-black w-7 h-7 rounded-full flex items-center justify-center">A</p>
             </div>
         </div>
+        <div className="flex items-center gap-2 mt-4 absolute top-17 left-[20rem] flex items-center gap-4 p-4">
+            <p className="bg-white text-black px-4 py-1 rounded-full curson-pointer">All</p>
+            <p className="text-white px-4py-1 rounded-2*1 cursor-pointer">Music</p>
+            <p className="text-white px-4 py-1 rounded-2*1 cursor-pointer">Podcast</p>
+        </div>
+
         </>
     )
 }
